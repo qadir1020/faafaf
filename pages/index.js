93099ce73d838aa03c45b3865838037a9fe4417f
Abdul Qadir <qadirabbasi1020@gmail.com>
2022-01-14 14:50:08 +0500

    new changes in newsletter

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,52 @@
 
-import { Page, Layout, EmptyState, Button, Card,TextField } from "@shopify/polaris";
+import { Page, Layout, EmptyState, Button, Card, TextField } from "@shopify/polaris";
 import { ResourcePicker, TitleBar } from '@shopify/app-bridge-react';
 
 class Index extends React.Component {
 
   render() {
-    state = { open: false}
+    state = { open: false,
+      value: '',
+    }
     return (
       <Page
-        title = "Product Selector"
+        title="Product Selector"
         primaryAction={{
           content: 'Select Products',
           onAction: () => this.setState({ open: true }),
         }}
-        
-        >
-        
-        <ResourcePicker
+
+      >
+
+        {/* <ResourcePicker
           resourceType="Product"
           open = {this.state.open}
           onCancel = {() => this.setState({ open: false })}
           onSelection = {(resources) => { this.handleSelection(resources) }}
-          />
+          /> */}
+
+        <TextField
+          label="Store name"
+          value={value}
+          onChange={handleChange}
+          autoComplete="off"
+        />
 
       </Page>
     );
   }
-    
+
   handleSelection = (resources) => {
     this.setState({ open: false });
     console.log(resources);
   };
+
+  handleChange = (value) => {
+
+    this.setState({ value: event.target.value });
+  
+}
+
 }
 
 export default Index;
